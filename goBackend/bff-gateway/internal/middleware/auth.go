45bcfa9d5acbf3a3fd60304d3dd550fd0003fe04
banana82orange <jirawat.kri@ku.th>
2026-01-03 04:29:12 +0700

    edit to accept all frontend url

--- goBackend/bff-gateway/internal/middleware/auth.go
+++ goBackend/bff-gateway/internal/middleware/auth.go
@@ -72,7 +72,7 @@ func RoleMiddleware(allowedRoles ...string) gin.HandlerFunc {
 // CORSMiddleware handles CORS
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "http://localhost:5004")
+		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
 		c.Header("Access-Control-Allow-Credentials", "true")
